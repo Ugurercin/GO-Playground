6a3b7842e68348a9d7d8e66a48e7f9ecc5fcaa69
Uğur Erçin <ugur@Ugurs-MacBook-Air.local>
2023-12-18 20:19:07 +0300

    pointers practiced

--- Go-Basics/002-Structs/main.go
+++ Go-Basics/002-Structs/main.go
@@ -1,1 +1,28 @@
 package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
+func main() {
+	Ugur := person{firstName: "Uğur", lastName: "Erçin", contact: contactInfo{email: "ercinugur@gmail.com", zipCode: 34500}}
+
+	Ugur.updateName("UğurPointer")
+	Ugur.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+func (p person) print() {
+
+	fmt.Printf("%v", p)
+}
